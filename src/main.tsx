13073cd64f6df4d006c6129e4d25b005448d57ea
Khalid Jameer <kjameer0@gmail.com>
2023-07-10 19:01:23 -0400

    chore: finished routing main pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,114 @@ import App from "./App";
 import "./index.css";
 import { ErrorPage } from "ErrorPage";
 import { createGlobalStyle } from 'styled-components';
+import {
+  About,
+  Admissions,
+  Contact,
+  ContactForm,
+  CounselorCorner,
+  Enrollment,
+  HomePage,
+  InfoRequestForm,
+  MissionSociety,
+  Parents,
+  SharedAdmissions,
+  Staff,
+  StudentCorner,
+  TeacherHub,
+} from './pages';
+import { baseUrl } from 'project-constants';
 
-const router = createBrowserRouter([{
-  path: '',
-  element: <App/>,
-  errorElement: <ErrorPage/>,
-  children: []
-}])
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: baseUrl, element: <HomePage />, errorElement: <ErrorPage />, children: [] },
+      { path: baseUrl + 'about', element: <About />, errorElement: <ErrorPage />, children: [] },
+      {
+        path: baseUrl + 'admissions',
+        element: <Admissions />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'contact',
+        element: <Contact />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'counselor-contact-form',
+        element: <ContactForm />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      // ADD in props for contact forms!!!
+      {
+        path: baseUrl + 'site-admin-contact-form',
+        element: <ContactForm />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'counselor-corner',
+        element: <CounselorCorner />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'enrollment',
+        element: <Enrollment />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'info-request-form',
+        element: <InfoRequestForm />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'mission-society',
+        element: <MissionSociety />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'parents-families',
+        element: <Parents />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'shared-admissions',
+        element: <SharedAdmissions />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'staff-directory',
+        element: <Staff />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'student-corner',
+        element: <StudentCorner />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+      {
+        path: baseUrl + 'teacher-hub',
+        element: <TeacherHub />,
+        errorElement: <ErrorPage />,
+        children: [],
+      },
+    ],
+  },
+]);
 
 const GlobalStyle = createGlobalStyle<{ $whiteColor?: boolean; $blackBackground?: boolean }>`
   :root {
