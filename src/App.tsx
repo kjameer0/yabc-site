8d81ff5e0451ef97d7e1019a716099423c14e46c
Khalid Jameer <kjameer0@gmail.com>
2023-07-31 13:43:48 -0400

    fix: edited text in banner

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <div className="banner-wrapper">
         <span className="banner">
-          **** Pre-Registration for Fall Classes will START August 21, 2023 and END on September 5, 2023 <span className='banner-bullet'>●</span> Fall
+          **** Pre-Registration for Fall Classes START August 21, 2023 and END September 5, 2023 <span className='banner-bullet'>●</span> Fall
           Classes start September 7, 2023<span className='banner-bullet'>●</span> In-Person Registration for Fall Classes will continue through
           September ****
         </span>
