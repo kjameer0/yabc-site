1c8dcee7f4629d61ee47ae3acdf133eef804b0a9
Khalid Jameer <kjameer0@gmail.com>
2023-11-03 17:42:57 -0400

    feat: set app.tsx to use json banner text

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,16 @@
-import { useEffect, useState } from 'react';
 //components
 import MobileNavBar from 'components/MobileNavBar';
 import SideNavBar from 'components/SideNavBar';
 import Footer from 'components/Footer';
 import { Outlet } from 'react-router-dom';
 //utils
-import { getBannerText } from 'utils/contentful-api-functions';
 //error
-import { errorGenerator } from 'utils/error';
 import { bannerText } from 'page-data/bannerData.json';
 
 //nav bar is for mobile view and side nav bar is for desktop/tablet
 //the <Outlet /> replaces the <main> tag on screen with the current page
 
 function App() {
-  const [bannerText, setBannerText] = useState('');
-  useEffect(() => {
-    try {
-      getBannerText().then((data) => {
-        if (!data) {
-          throw new ReferenceError('no banner data');
-        }
-        setBannerText(data);
-      });
-    } catch (error: unknown) {
-      errorGenerator(error)
-    }
-  }, []);
   return (
     <>
       <div className="banner-wrapper">
